cfddcc0d5b008fbfa6ec0031b05795e157740f36
Melanie Taylor <taylor.melanie808@gmail.com>
2022-10-17 20:15:40 -0400

    mod R Script with Summary table and T-Test

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,29 @@
 install.packages("tidyverse")
 install.packages("dplyr")
+library(dplyr)
+
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F) # Import the csv file
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) 
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,MechaCar) ) #run the Linear regression model
+
+# Import the csv file
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#create summary table
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+#lot summary
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups = 'keep')
+
+
+# t-test for all lots
+t.test(Suspension_Coil$PSI,mu=mean(Suspension_Coil$PSI))
+
+# t-test for lot 1
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot1"),mu=mean(Suspension_Coil$PSI))
+
+# t-test for lot 2
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot2"),mu=mean(Suspension_Coil$PSI))
+
+# t-test for lot 3
+t.test(subset(Suspension_Coil$PSI,Suspension_Coil$Manufacturing_Lot == "Lot3"),mu=mean(Suspension_Coil$PSI))
